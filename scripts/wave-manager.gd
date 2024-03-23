b21e8ecc27029a9e10a08a9c902f57ec362ded98
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-03-23 07:31:04 +0700

    Add template for endless mode in wave manager
    - If wave number >= 16, call endless mode

--- scripts/wave-manager.gd
+++ scripts/wave-manager.gd
@@ -1,8 +1,10 @@
 extends Node
 
 var is_start = false
-var wave_number = 0
-var wave_timer: Timer = null
+@export var is_endless_mode = false
+@export var wave_number = 0
+@export var wave_timer: Timer = null
+@export var multiplier: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():	
@@ -50,3 +52,37 @@ func _deffered_next_wave():
 		
 		# Do normal wave spawner here
 		wave_timer.start()
+		
+func endless_mode_next_wave():
+	call_deferred("_deferred_endless_mode_next_wave")
+	
+func _deferred_endless_mode_next_wave():
+	#
+	# Call different function if desired
+	#
+	if (wave_number % 5 == 0):
+		print("endless mode boss wave")
+		
+		# Do boss wave spawner here
+		# Delete this if the boss has been implemented
+		await get_tree().create_timer(5.0).timeout
+		boss_death()
+		# Delete until this
+	else:
+		print("endless mode normal wave")
+		
+		# Do normal wave spawner here
+		wave_timer.start()
+
+func boss_death():
+	call_deferred("_deferred_boss_death")
+	
+func _deferred_boss_death():
+	if wave_number % 5 == 0:
+		next_wave()
+
+func calculate_multiplier():
+	call_deferred("_deferred_calculate_multiplier")
+	
+func _deferred_calculate_multiplier():
+	multiplier = wave_number
