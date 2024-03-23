5cda8462a2c75eb81428e5f7129645f5995adcae
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-03-23 07:50:47 +0700

    Update Misc UI
    - Using Skranji Font
    - Update time left with ceiling to 1 decimal point
    - Add is_boss_wave for convenient regarding boss wave

--- scripts/wave-manager.gd
+++ scripts/wave-manager.gd
@@ -35,7 +35,10 @@ func _deffered_next_wave():
 	calculate_multiplier()
 	SpawnManager.new_wave(wave_number)
 	
-	if (wave_number % 5 == 0):
+	if (wave_number >= 16):
+		endless_mode_next_wave()
+	elif (wave_number % 5 == 0):
+		is_boss_wave = true
 		print("boss wave")
 		
 		# Do boss wave spawner here
@@ -46,6 +49,29 @@ func _deffered_next_wave():
 	else:
 		is_boss_wave = false
 		print("normal wave")
+		
+		# Do normal wave spawner here
+		wave_timer.start()
+		
+func endless_mode_next_wave():
+	call_deferred("_deferred_endless_mode_next_wave")
+	
+func _deferred_endless_mode_next_wave():
+	#
+	# Call different function if desired
+	#
+	if (wave_number % 5 == 0):
+		is_boss_wave = true
+		print("endless mode boss wave")
+		
+		# Do boss wave spawner here
+		# Delete this if the boss has been implemented
+		await get_tree().create_timer(5.0).timeout
+		boss_death()
+		# Delete until this
+	else:
+		is_boss_wave = false
+		print("endless mode normal wave")
 		
 		# Do normal wave spawner here
 		wave_timer.start()
