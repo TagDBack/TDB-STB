a0bc042d6e9982d5c84735d032774c521a772710
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-04-20 09:51:56 +0700

    Create spawner simulation using simple cube

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var enemy_pack: PackedScene
 @export var total_spawn_number: int = 100
 @export var total_batch: int = 5
-@export var delay_batch_spawn = 5
+@export var delay_batch_spawn = 0.1
 @export var min_radius = 0
 @export var max_radius = 5
 @export var origin = Vector3(0, 0, 0)
@@ -23,9 +23,10 @@ func _deferred_spawn(number_spawn_now):
 		
 		var spawn_pos = origin
 		var angle = randf_range(-2 * PI, 2 * PI)
+		var radius = randf_range(min_radius, max_radius)
 		
-		spawn_pos.x = spawn_pos.x + randf_range(min_radius, max_radius) * cos(angle)
-		spawn_pos.z = spawn_pos.z + randf_range(min_radius, max_radius) * sin(angle)
+		spawn_pos.x = spawn_pos.x + radius * cos(angle)
+		spawn_pos.z = spawn_pos.z + radius * sin(angle)
 		
 		spawned.global_position = spawn_pos
 		spawned.look_at(origin)
