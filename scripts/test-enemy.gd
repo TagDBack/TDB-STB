f672384f35a55fb5301c0d0e8ce83d2ff6a3f0ae
Linus <49267246+ZhouZhiLiang@users.noreply.github.com>
2024-04-25 04:53:27 +0700

    Drops WIP, added barebones lvl 1

--- scripts/test-enemy.gd
+++ scripts/test-enemy.gd
@@ -8,30 +8,30 @@ extends RigidBody3D
 var isDead = false
 
 @export var expDrop = 10
-@export var goldRate = 0.01
 
-signal death
+signal death(expDrop)
+signal drop()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func take_damage(damage):
+	#print("dmg")
 	var damageTaken = damage - armor
 	health -= damageTaken
 	if health <= 0:
+		drop.emit(global_position)
+		$dead.start()
+		print("die")
 		isDead = true
 		death.emit(expDrop)
 
-func drop_gold():
-	var rng
-	
-	if rng <= goldRate:
-		pass #drop gold
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+	#if Input.is_action_just_pressed("action_melee"):
+		##print("dmg")
+		#take_damage(5)
 
 func _on_dead_timeout():
-	pass # remove self
+	queue_free()
