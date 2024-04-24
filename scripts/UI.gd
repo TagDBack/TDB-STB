0e769a8240e60cbd65e3a1c0bddd8f52724f44c0
rffp <88695295+rafifpriyo@users.noreply.github.com>
2024-04-24 17:53:28 +0700

    Primary/wave/wave UI fif v3 (#3)
    Wave UI and manager

---
+++ scripts/UI.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+var container
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	container = $"MarginContainer/HBoxContainer/VBoxContainer2"
+	await get_tree().create_timer(1).timeout
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	container.get_node("Wave Number").text = "Wave " + str(WaveManager.wave_number)
+	if WaveManager.is_boss_wave:
+		container.get_node("Wave Time Left").text = "Defeat The Boss"
+	else:
+		var time_left = int(ceil(WaveManager.wave_timer.time_left))
+		container.get_node("Wave Time Left").text = str(int(time_left / 60)) + ' : ' + str(time_left % 60)
+
+# https://forum.godotengine.org/t/how-to-round-to-a-specific-decimal-place/27552/3
+func ceil_to_dec(num, digit):
+	return ceil(num * pow(10.0, digit)) / pow(10.0, digit)
---
+++ scripts/wave-manager.gd
@@ -0,0 +1,92 @@
+extends Node
+
+var is_start = false
+@export var is_boss_wave = false
+@export var is_endless_mode = false
+@export var wave_number = 0
+@export var wave_timer: Timer = null
+@export var multiplier: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():	
+	start()
+
+func start():
+	call_deferred("_deferred_start")
+	
+func _deferred_start():
+	if is_start:
+		return
+	
+	is_start = true
+	multiplier = 1
+	
+	var t = ResourceLoader.load("res://scenes/miscs/wave-timer.tscn")
+	var timer = t.instantiate()
+	wave_timer = timer
+	get_tree().root.add_child(timer)
+	
+	next_wave()
+	
+func next_wave():
+	call_deferred("_deffered_next_wave")
+	
+func _deffered_next_wave():
+	wave_number += 1
+	print(wave_number)
+	calculate_multiplier()
+	
+	if (wave_number >= 16):
+		endless_mode_next_wave()
+	elif (wave_number % 5 == 0):
+		is_boss_wave = true
+		print("boss wave")
+		
+		# Do boss wave spawner here
+		# Delete this if the boss has been implemented
+		await get_tree().create_timer(5.0).timeout
+		boss_death()
+		# Delete until this
+	else:
+		is_boss_wave = false
+		print("normal wave")
+		
+		# Do normal wave spawner here
+		wave_timer.start()
+		
+func endless_mode_next_wave():
+	call_deferred("_deferred_endless_mode_next_wave")
+	
+func _deferred_endless_mode_next_wave():
+	#
+	# Call different function if desired
+	#
+	if (wave_number % 5 == 0):
+		is_boss_wave = true
+		print("endless mode boss wave")
+		
+		# Do boss wave spawner here
+		# Delete this if the boss has been implemented
+		await get_tree().create_timer(5.0).timeout
+		boss_death()
+		# Delete until this
+	else:
+		is_boss_wave = false
+		print("endless mode normal wave")
+		
+		# Do normal wave spawner here
+		wave_timer.start()
+
+func boss_death():
+	call_deferred("_deferred_boss_death")
+	
+func _deferred_boss_death():
+	if wave_number % 5 == 0:
+		is_boss_wave = false
+		next_wave()
+
+func calculate_multiplier():
+	call_deferred("_deferred_calculate_multiplier")
+	
+func _deferred_calculate_multiplier():
+	multiplier = wave_number
