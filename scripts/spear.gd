a7d60be234639899f76f1998c43c25fec51e0a20
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-05-07 08:25:48 +0700

    Create spear for melee-weapon
    - It's OP, just sweep with mouse, and it's done
    - Make it weaker by lessing the damage

---
+++ scripts/spear.gd
@@ -0,0 +1,45 @@
+extends melee_weapon
+
+#@onready var area_collision = $Area3D
+#
+#@export var speed = 20
+#@export var damage = 10
+#
+#@onready var origin_rotation = self.transform
+
+#@onready var rotate_to = self.transform.basis
+#var is_rotating = false
+
+var stretch_length = 0
+var stretch_speed = 15
+@export var max_strecth = 3
+# From 0 to max_strecth
+
+func _ready():
+	area_collision.body_entered.connect(_enemy_on_body_entered)
+	stretch_speed = speed
+	damage = 0.4 * damage
+
+func _physics_process(delta):
+	if stretch_length == 0:
+		change_visibility(false)
+	else:
+		change_visibility(true)
+	
+	if Input.is_action_pressed("action_melee"):
+		if stretch_length != max_strecth:
+			stretch_length = min(max_strecth, stretch_length + stretch_speed * delta)
+			self.position.z = - stretch_length
+	else:
+		if stretch_length != 0:
+			stretch_length = max(0, stretch_length - stretch_speed * delta)
+			self.position.z = - stretch_length
+
+#func change_visibility(trufals):
+	#self.visible = trufals
+	#self.get_node("Area3D").monitoring = trufals
+	#
+	#
+#func _enemy_on_body_entered(body_entered):
+	#if body_entered is enemy:
+		#body_entered.take_damage(damage)
