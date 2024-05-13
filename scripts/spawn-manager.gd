ca8f509fc9c0a605f69fc98ef1577837219307e8
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-05-13 18:45:16 +0700

    Integrate mode selection with player's weapon

--- scripts/spawn-manager.gd
+++ scripts/spawn-manager.gd
@@ -11,6 +11,7 @@ func new_wave(wave_number):
 func _deferred_new_wave(wave_number):
 	if wave_number == 1:
 		spawn("res://scenes/spawners/wave1.tscn")
+		assign_weapons()
 	elif wave_number == 2:
 		spawn("res://scenes/spawners/wave2.tscn")
 	else:
@@ -24,3 +25,24 @@ func _deferred_spawn(spawner_level_path):
 	var s = ResourceLoader.load(spawner_level_path)
 	var spawner = s.instantiate()
 	get_tree().root.add_child(spawner)
+
+func assign_weapons():
+	call_deferred("_deferred_assign_weapons")
+
+func _deferred_assign_weapons():
+	await get_tree().create_timer(0.1).timeout
+	var player = get_tree().get_nodes_in_group("player")
+	if len(player) == 0:
+		player = null
+	else:
+		player = player[0]
+	
+	var melee_weapon_node = null
+	if ResourceManager.melee_weapon_selected == "Sword":
+		melee_weapon_node = ResourceLoader.load("res://scenes/entities/melee-weapon/sword.tscn").instantiate()
+	elif ResourceManager.melee_weapon_selected == "Spear":
+		melee_weapon_node = ResourceLoader.load("res://scenes/entities/melee-weapon/spear.tscn").instantiate()
+	elif ResourceManager.melee_weapon_selected == "Shield":
+		melee_weapon_node = ResourceLoader.load("res://scenes/entities/melee-weapon/shield.tscn").instantiate()
+	
+	player.get_node("Entity").add_child(melee_weapon_node)
