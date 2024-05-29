b44223f7710c68df3515b17b811ee8a970b3739e
Rafif Priyo <muhammad.rafif01@ui.ac.id>
2024-05-29 11:18:04 +0700

    Rebase 0.9.5 to 0.9.4 proper

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,32 +1,22 @@
 extends Node3D
 
+@export var player: CharacterBody3D
+
 @export var enemy_pack: PackedScene
-@export var total_spawn_number: int = 100
+@export var total_spawn_number: int = 15
 @export var total_batch: int = 5
-@export var delay_batch_spawn = 0.1
+@export var delay_batch_spawn = 1
 @export var min_radius = 0
 @export var max_radius = 5
 @export var origin = Vector3(0, 10, 0)
 
 func _ready():
 	randomize()
+	scan_player()
 	repeat()
 
-func repeat():
-	call_deferred("_deferred_repeat")
-
-func _deferred_repeat():
-	if total_batch == 0:
-		self.queue_free()
-	else:
-		var number_spawn_now = floor(total_spawn_number / total_batch)
-		GlobalFuncs_.enemies_left += number_spawn_now
-		spawn(number_spawn_now)
-		total_spawn_number = total_spawn_number - number_spawn_now
-		total_batch = total_batch - 1
-		
-		await get_tree().create_timer(delay_batch_spawn).timeout
-		repeat()
+func _setup(player):
+	self.player = player
 
 func spawn(number_spawn_now):
 	call_deferred("_deferred_spawn", number_spawn_now)
@@ -39,9 +29,36 @@ func _deferred_spawn(number_spawn_now):
 		var angle = randf_range(-2 * PI, 2 * PI)
 		var radius = randf_range(min_radius, max_radius)
 		
+		# Add the spawned node to the scene tree first
+		get_parent().get_parent().add_child(spawned)
 		spawn_pos.x = spawn_pos.x + radius * cos(angle)
 		spawn_pos.z = spawn_pos.z + radius * sin(angle)
 		
 		spawned.global_position = spawn_pos
+		spawned.look_at(origin)
 		
-		get_parent().get_parent().add_child(spawned)
+		if player != null:
+			spawned.player = player
+			#spawned.isHunt = true
+
+func repeat():
+	call_deferred("_deferred_repeat")
+
+func _deferred_repeat():
+	if total_batch == 0:
+		self.queue_free()
+	else:
+		var number_spawn_now = floor(total_spawn_number / total_batch)
+		spawn(number_spawn_now)
+		total_spawn_number = total_spawn_number - number_spawn_now
+		total_batch = total_batch - 1
+		
+		await get_tree().create_timer(delay_batch_spawn).timeout
+		repeat()
+	
+func scan_player():
+	var list_player = get_tree().get_nodes_in_group("player")
+	if len(list_player) == 0:
+		player = null
+	else:
+		player = list_player[0]
